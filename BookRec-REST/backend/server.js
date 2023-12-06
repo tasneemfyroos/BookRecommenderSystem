737b31089d4e06df807cc3c28cffff3747f013d0
tasneemfyroos <135737150+tasneemfyroos@users.noreply.github.com>
2023-12-05 22:22:01 -0600

    mysql and defined get and post endpoints

--- BookRec-REST/backend/server.js
+++ BookRec-REST/backend/server.js
@@ -1,8 +1,46 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
 
+const bookdb = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "bookrec",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  bookdb.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `author`, `genre`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.author,
+    req.body.genre,
+  ];
+
+  bookdb.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
 
 app.listen(8000, () => {
-  console.log("Connected to server!");
+  console.log("Connected to server");
 });
